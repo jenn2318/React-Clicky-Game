4d024b806d8ebbfb5494e685aa7e827a93cbe450
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-06-25 19:02:28 -0400

    rafactored code

--- ClickyGame/src/App.js
+++ ClickyGame/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 class App extends Component {
     state = {
-        flowers:flowers,
+        flowers,
         currentScore:0,
         highScore:0,
         gameText:'Click any picture to start the game!',
@@ -16,75 +16,100 @@ class App extends Component {
     };
 
     //This is the shuffle for loop
-    shuffle = (f) => {
-        for (let i = f.length - 1; f > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [f[i], f[j]] = [f[j], f[i]];
-        }
-        return f;
+
+    shuffle = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+    i--;
+}
+return data;
+
+
     }
 
     //This code will handle when the flower picture is clicked....
     handleClickedFlower = (id) => {
-        let newFlowers = this.state.flowers;
+        console.log(id, "handleClickedFlower");
+        let newFlowers = [...this.state.flowers];
         const checkFlower = newFlowers.indexOf(newFlowers.find(flower => flower.id === id));
-        if((newFlowers[checkFlower].touched === 0) && (this.state.status === 0))
+        console.log(newFlowers[checkFlower]);
+        if(newFlowers[checkFlower].clicked === 0)
         {
-            newFlowers[checkFlower].checkGoodBad = "card is Good";
+            //console.log(newFlowers[checkFlower]);
             newFlowers[checkFlower].clicked = 1;
-            newFlowers[checkFlower].checkGoodBad = "";
-            this.shuffle(newFlowers);
-            this.setState({flowers:newFlowers});
-            this.handleCurrentScore();
+
+            this.setState({
+                flowers:this.shuffle(newFlowers),
+                currentScore: this.state.currentScore + 1,
+                highScore: this.handleHighScore()
+            });
+
+            //this.handleCurrentScore();
         }
-        else if (this.state.status === 0)
+        else
         {
-            newFlowers[checkFlower].checkGoodBad = "card is Bad";
-            this.setState({status:1});
-            this.youLose();
+            //If a user clicks on the image more than once the score should be reset to 0
+            //The state should reset to shuffle the flowers
+            console.log("the click is wrong", this.state.flowers);
+
+
+            this.setState({
+
+                flowers: this.shuffle(flowers),
+                currentScore: 0
+            }, () => {console.log("state was updated", this.state.flowers) } )
+
         }
 
     }
 //Code to let the user see their high score of every game
-    handleHighScore = (score) => {
-        const newHighScore = score;
-        if(score > this.state.highScore)
-            this.setState({highScore:newHighScore});
-        if(score === 12){
-            this.setState({status:1});
-            this.youWin()
-        }
+//It should check the high score versus the current score
+//Return whichever score is higher
+
+    handleHighScore  = () =>
+{
+    if (this.state.currentScore > this.state.highScore) {
+        return this.state.currentScore
     }
- //The user will see their current score
-    handleCurrentScore = () => {
-        const newScore = this.state.currentScore + 1;
-        this.setState({currentScore:newScore});
-        this.handleHighScore(newScore);
+
+   else  {
+        return this.state.highScore
     }
-   //This will reset the flowers in order
-    resetFlowers = () => {
-        let newFlowers = this.state.flowers;
-        newFlowers.forEach(function(item,index){
-            item.clicked = 0;
-            item.goodBad = "";
-        });
-        this.setState({flowers:newFlowers});
-    }
-//Reset the game to start over
-    resetGame = () => {
-        this.setState({currentScore:0});
-        this.setState({status:0})
-        this.setState({gameText:"Click a picture to start the game!"})
-        this.resetFlowers();
-    }
-//Loss Message
-    youLose = () => {
-        this.setState({gameText:"You Lose! (Try again and reset)"});
-    }
-//Win Message
-    youWin = () => {
-        this.setState({gameText:"You Win! (Play Again and reset)"});
-    }
+}
+//  //The user will see their current score
+//     handleCurrentScore = () => {
+//         const newScore = this.state.currentScore + 1;
+//         this.setState({currentScore:newScore});
+//         this.handleHighScore(newScore);
+//     }
+//    //This will reset the flowers in order
+//     resetFlowers = () => {
+//         let newFlowers = this.state.flowers;
+//         newFlowers.forEach(function(item,index){
+//             item.clicked = 0;
+//             item.goodBad = "";
+//         });
+//         this.setState({flowers:newFlowers});
+//     }
+// //Reset the game to start over
+//     resetGame = () => {
+//         this.setState({currentScore:0});
+//         this.setState({status:0})
+//         this.setState({gameText:"Click a picture to start the game!"})
+//         this.resetFlowers();
+//     }
+// //Loss Message
+//     youLose = () => {
+//         this.setState({gameText:"You Lose! (Try again and reset)"});
+//     }
+// //Win Message
+//     youWin = () => {
+//         this.setState({gameText:"You Win! (Play Again and reset)"});
+//     }
 
 
 
@@ -95,7 +120,7 @@ class App extends Component {
                 <NavBar
                     highScore = {this.state.highScore}
                     currentScore = {this.state.currentScore}
-                    resetGame = {this.resetGame}
+                    //resetGame = {this.resetGame}
                     gameMessage = {this.state.gameText}
                 />
                 <h2 className="title">Tropical Flower Clicky Game!</h2>
